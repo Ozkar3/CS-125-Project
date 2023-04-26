c2eed427a91a0c32bace24f45886df8629b4638b
Oscar Gabriel Zaragoza <zaragozo@uci.edu>
2023-04-25 21:56:14 -0700

    Initial Commit

---
+++ Happy Panda/Happy_PandaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Happy_PandaApp.swift
+//  Happy Panda
+//
+//  Created by Oscar Zaragoza on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Happy_PandaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
