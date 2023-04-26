7696611005a61db724a6e46065c8aa252f78b10c
Oscar Gabriel Zaragoza <zaragozo@uci.edu>
2023-04-26 02:19:36 -0700

    skeleton log in screen

--- Happy Panda/ContentView.swift
+++ Happy Panda/ContentView.swift
@@ -8,19 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing:30) {
+            Text("HAPPY PANDA")
+                .font( .system(size:45))
+                .fontWeight(.heavy)
+                .multilineTextAlignment(.center)
+                .offset(y:-10)
+            
+            TextField(
+                    "User name",
+                    text: $username)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .padding(.all,15)
+                    //.padding(.trailing,35)
+                    .border(.black,width: 5)
+                    .frame(width: 315)
+                    //.offset(y:50)
+            SecureField(
+                    "Password",
+                    text: $password)
+            .padding(.all,15)
+            //.padding(.trailing,35)
+            .border(.black,width: 5)
+            .frame(width: 315)
+            //.offset(y:50)
+            
+            Button {
+                
+            } label: {
+                Text("Log In")
+                    .foregroundColor(.white)
+                    //.bold()
+                    .fontWeight(.heavy)
+                    .frame(width: 150, height: 45)
+                    .background(RoundedRectangle(cornerRadius: 5)
+                    ).foregroundColor(.black)
+                    //.offset(y:80)
+                }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+               
+            Image("panda")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .offset(y:100)
         }
-        .padding()
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
