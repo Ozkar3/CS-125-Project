74f6dc5c97c8bd59d07ad0ab163d2e397ab59463
Oscar Gabriel Zaragoza <zaragozo@uci.edu>
2023-06-07 14:19:01 -0700

    display sleep and steps

---
+++ Happy Panda/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  TryHealthKit
+//
+//  Created by Jonathan Ricky Sandjaja on 07/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject var vm: HealthKitViewModel
+    
+    var body: some View {
+        VStack {
+            if vm.isAuthorized {
+                VStack {
+                    Text("Today's Step Count")
+                        .font(.title3)
+                    
+                    Text("\(vm.userStepCount)")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    
+                    if let sleepDuration = vm.userSleepDuration {
+                        Text("Today's Sleep Duration")
+                            .font(.title3)
+                        
+                        Text("\(sleepDuration)")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                    } else {
+                        Text("No sleep data available")
+                            .font(.title3)
+                    }
+                }
+            } else {
+                VStack {
+                    Text("Please Authorize Health!")
+                        .font(.title3)
+                    
+                    Button {
+                        vm.healthRequest()
+                    } label: {
+                        Text("Authorize HealthKit")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                    }
+                    .frame(width: 320, height: 55)
+                    .background(Color(.orange))
+                    .cornerRadius(10)
+                }
+            }
+            
+        }
+        .padding()
+        .onAppear {
+            vm.readStepsTakenToday()
+            vm.readSleepDuration()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(HealthKitViewModel())
+    }
+}
+
