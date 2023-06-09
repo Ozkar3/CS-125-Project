3e2a5cf8bc93ee6b6730cdb039d55297b19ea38b
mmrafael <74680714+mmrafael@users.noreply.github.com>
2023-06-09 07:00:37 -0700

    Update Sleep.swift

--- Happy Panda/Pages/Sleep.swift
+++ Happy Panda/Pages/Sleep.swift
@@ -5,16 +5,37 @@
 //  Created by Maritza Pott on 5/2/23.
 //
 
+
 import SwiftUI
+import Firebase
+import FirebaseFirestore
+import FirebaseDatabase
 
 struct Sleep: View {
+    @EnvironmentObject var vm: HealthKitViewModel
     @State var sleepTime = Date()
     @State var wakeUpTime = Date()
     @State var totalHours = 0
     @State var totalMinutes = 0
+    @State var min = 0
+    @State var max = 0
+    func ageCalc(){
+        if let age = vm.userAge {
+            if (13 <= age && age < 19){
+                min = 8
+                max = 10
+            }
+            else if (age >= 19){
+                min = 7
+                max = 9
+            }
+        }
+    }
     let calendar = Calendar.current
     @EnvironmentObject var firestoreManager: FirestoreManager
     
+    
+    private let database = Database.database().reference()
     
     
     
@@ -51,6 +72,11 @@ struct Sleep: View {
         
 
     }
+//    func recommendedPath() {
+//        if (13 <= age && age < 19){
+//            RecommendationResultsTeens
+//        }
+//    }
         
 
     var datePickers: some View {
@@ -90,7 +116,30 @@ struct Sleep: View {
         totalMinutes = totalTimePassed.minute ?? 0
         
         
+        
+//        let docData: [String: Any] = [
+//            "sleep_avg": 0,
+//            "sleep_hours": totalHours,
+//            "sleep_total": totalTimePassed,
+//        ]
+//        let db = Firestore.firestore()
+//
+//        let docRef = db.collection("sleep").document(firestoreManager.userUID)
+//
+//        docRef.setData(docData) { error in
+//            if let error = error {
+//                print("Error writing document: \(error)")
+//            } else {
+//                print("Document successfully written!")
+//            }
+//        }
     }
+    
+    
+    
+    
+   
+    
     
     var RecommendationResults: some View{
         
@@ -99,37 +148,39 @@ struct Sleep: View {
         
         VStack(alignment: .center){
         
-            if (totalHours == 7){
+            
+            
+            if (min <= totalHours && totalHours < max){
                 Section{
                     Image("happypanda")
                         .resizable()
                         .frame(width: 110.0, height: 100.0)
-                    Text("good sleep!")
+                    Text("You are getting a good amount of sleep! You should aim to sleep for \(min) - \(max) hours")
                         .position(x:190, y:10)
                 }
                 .offset(x:0, y:10)
                     
             }
-            else if (1 <= totalHours && totalHours < 7){
+            else if (0 <= totalHours && totalHours < min){
                 
                 Section{
                     Image("sleepypanda")
                         .resizable()
                         .frame(width: 110.0, height: 100.0)
                     Spacer()
-                    Text("Needs Improvement. You need 7 hours of sleep for a great rest")
+                    Text("Needs Improvement. You need \(min) - \(max) hours of sleep for a great rest")
                         .padding(5.0)
                         .position(x:190, y:10)
                 }
                 .offset(x:0, y:10)
             }
-            else if (totalHours > 7){
+            else if (totalHours > max){
                 
                 Section{
                     Image("tiredpanda")
                         .resizable()
                         .frame(width: 110.0, height: 100.0)
-                    Text("Thats too much sleep! Try sleeping within the recommended amount")
+                    Text("Thats too much sleep! Try sleeping within the recommended amount of \(min) - \(max)")
                         .padding()
                         .position(x:190, y:10)
                 }
@@ -145,12 +196,15 @@ struct Sleep: View {
         
     }
     
+    
 
 }
 
 struct Sleep_Previews: PreviewProvider {
     static var previews: some View {
-        Sleep()
+        Sleep().environmentObject(HealthKitViewModel())
     }
 }
+
+
 
