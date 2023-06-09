c651b4def98496e431b2cfe6f34971142a456381
mmrafael <74680714+mmrafael@users.noreply.github.com>
2023-06-09 16:18:42 -0700

    Update NavigationBar.swift

--- Happy Panda/NavigationBar.swift
+++ Happy Panda/NavigationBar.swift
@@ -7,61 +7,103 @@
 
 import SwiftUI
 
-struct NavigationBar: View {
-    @State var tabSelection = 1
-    
-    @EnvironmentObject var firestoreManager: FirestoreManager
-    @StateObject private var sharedData = SharedData()
+struct Food: View {
+    let coreDM: DataController
+    @State private var foodlist: [FoodTracker] = [FoodTracker]()
     
     
+    @State var name = ""
+    @State var calories:Double = 0
     
     var body: some View {
-        TabView(selection: $tabSelection) {
-            
-            Home(tabSelection: $tabSelection)
-                .tabItem(){
-                    Image(systemName: "house.fill")
-                    Text("Home")
+       
+        VStack{
+            HStack {
+
+                Text("Food Diary")
+                .font(.system(size:25))
+                .bold()
+            }
+            NavigationView{
+                VStack{
+                    Text("\(Int(totalCalories())) KCal Today")
+                        .foregroundColor(.black)
+                        .padding(.horizontal)
+
+
+                    Form{
+                        Section(header: Text("Log your Food!")){
+
+                            TextField("Food name", text: $name)
+                            VStack{
+                                Text("Calories: \(Int(calories))")
+                                Slider(value: $calories, in: 0...1500, step: 10)
+                            }
+                            .padding()
+
+                            HStack{
+                                Spacer()
+                                Button("Submit"){
+
+                                    coreDM.saveFood(name: name, calories: calories)
+                                    populateList()
+
+                                }
+
+                                Spacer()
+                            }
+
+                        }
+                        Section(header: Text("Your Entries")){
+                            List{
+                                ForEach(foodlist, id: \.self){
+                                    item in Text("\(item.name ?? ""): \(Int(item.calories))")
+                                }.onDelete(perform: {item in item.forEach{foods in let updatedfood = foodlist[foods]
+                                    coreDM.deleteFood(food: updatedfood)
+                                    populateList()
+                                }})
+                            }
+
+                        }
+//
+                    }
+
+
+                    Spacer()
+
+
                 }
-                .tag(1)
-            
-            Sleep()
-                .tabItem(){
-                    Image(systemName: "cloud.moon.fill")
-                    Text("Sleep")
-                }
-                .tag(2)
-            
-            Food(coreDM: DataController())
-                .tabItem(){
-                    Image(systemName: "carrot.fill")
-                    Text("Food")
-                }
-                .tag(3)
-            
-            Activity()
-                .environmentObject(sharedData)
-                .tabItem(){
-                    Image(systemName: "figure.run")
-                    Text("Activity")
-                }
-                .tag(4)
-            
-            Profile()
-                .environmentObject(sharedData)
-                .tabItem(){
-                    Image(systemName: "person.fill")
-                    Text("Profile")
-                }
-                .tag(5)
-            
+                .onAppear(perform: {
+                    foodlist = coreDM.getAllFoods()
+                })
+
+
+            }
         }
+    }
+        
+    
+    private func totalCalories() -> Double{
+        // figure out how to display total calories
+        var sumCalories: Double = 0
+        for item in foodlist {
+            if Calendar.current.isDateInToday(item.date!){
+
+                sumCalories += item.calories
+                
+            }
+        }
+
+        return sumCalories
+    }
+    private func populateList(){
+        foodlist = coreDM.getAllFoods()
     }
 }
 
-struct NavigationBar_Previews: PreviewProvider {
+struct Food_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationBar().environmentObject(FirestoreManager())
+        Food(coreDM: DataController())
     }
 }
 
